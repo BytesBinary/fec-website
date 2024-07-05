29be3ca8f7015d2ddd54b72178e2dde77c2930fa
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-07-05 23:23:55 +0600

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.app')
+
+@section('title')
+    হোম পেইজ
+@endsection
+
+@section('style')
+    <style>
+        /* Custom CSS */
+    </style>
+@endsection
+
+@section('content')
+    <div class="container">
+        <h1>
+            Hello, this is working
+        </h1>
+    </div>
+@endsection
+
+
+@section('script')
+    <script>
+        // Custom JS
+    </script>
+@endsection
