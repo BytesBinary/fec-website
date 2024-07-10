6635667e5fc8b531d035b4babc610aa29558bd89
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-07-10 11:42:48 +0600

    Update the header

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,9 @@
+<div class="container-mx">
+    <div class="row">
+        <div class="col-md-12 text-center p-3">
+            <div class="header">
+                <h1>Faridpur Engineering College</h1>
+            </div>
+        </div>
+    </div>
+</div>
