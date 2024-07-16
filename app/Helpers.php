3bc0fb409a2330038926c4f85c3975b86518a34f
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-07-16 15:15:36 +0600

    made home dynamically active

--- app/Helpers.php
+++ app/Helpers.php
@@ -3,6 +3,6 @@
 if( ! function_exists( 'is_active_route' ) ) {
     function is_active_route($route)
     {
-        return request()->routeIs($route) ? 'active' : '';
+        return request()->routeIs($route) ? 'active' : 'sdfsadfas';
     }
 }
