89b5c4c739c89c2550efaa89065ae6ef1032b3bf
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-08-12 23:44:20 +0600

    created contact us page

--- app/Http/Controllers/ContactUsPage/ContactUsPageController.php
+++ app/Http/Controllers/ContactUsPage/ContactUsPageController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers\ContactUsPage;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\PageMetas;
 
 class ContactUsPageController extends Controller
 {
     public function load_page()
     {
         $page = get_page_details('contactus');
+        $contact_details = json_decode( PageMetas::where('page_id', $page['id'])->where('meta_key', 'contact_details')->first()->meta_value, true);
         return view('contact-us', array(
-            'page' => $page
+            'page' => $page,
+            'contact_details' => $contact_details
         ));
     }
 }
