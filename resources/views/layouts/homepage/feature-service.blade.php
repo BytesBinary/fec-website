a84d1ad0067262575a043778ad502f55a1df4a80
SN Sabit <168827958+Sabit056@users.noreply.github.com>
2024-08-12 20:16:14 +0600

    homepage section dividing

---
+++ resources/views/layouts/homepage/feature-service.blade.php
@@ -0,0 +1,43 @@
+<!-- Featured Services Section -->
+<section id="featured-services" class="featured-services section light-background">
+
+    <div class="container">
+        <div class="row gy-4">
+            <!-- Start Service Items -->
+            <div class="col-xl-4 col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="service-item d-flex">
+                    <div class="icon flex-shrink-0"><i class="bi bi-mortarboard-fill" style="color: red"></i></div>
+                    <div class="col-md-3">
+                        <div class="h-about-body">
+                            <h4 style="color: red">Established</h4>
+                            <h3>2010</h3>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-6" data-aos="fade-up" data-aos-delay="200">
+                <div class="service-item d-flex">
+                    <div class="icon flex-shrink-0"><i class="bi bi-buildings" style="color: red"></i></div>
+                    <div>
+                        <h4 class="title"><a href="#" class="stretched-link" style="color: red ">Departments</a></h4>
+                        <h3>3</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-6" data-aos="fade-up" data-aos-delay="300">
+                <div class="service-item d-flex">
+                    <div class="icon flex-shrink-0"><i class="bi bi-people-fill" style="color: red"></i></div>
+                    <div>
+                        <h4 class="title"><a href="#" class="stretched-link" style="color: red ">Students</a></h4>
+                        <h3>800+</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Service Items -->
+    </div>
+
+</section><!-- /Featured Services Section -->
