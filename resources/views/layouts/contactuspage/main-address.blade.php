cca21e134cd282d299bc3f0c1b76fd7db048e502
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2024-08-13 20:21:06 +0600

    fixed main address and map

--- resources/views/layouts/contactuspage/main-address.blade.php
+++ resources/views/layouts/contactuspage/main-address.blade.php
@@ -1,3 +1,6 @@
-<x-contact-page-card row-class-md="6" row-class-lg="6" logo-class="bi bi-geo-alt" title="Address" description="Dr Kaji Motaher Hossain Rd,Komlapur,Baytul Aman,Faridpur-7800" />
-<x-contact-page-card row-class-md="6" row-class-lg="3" logo-class="bi bi-telephone" title="Call Us" description="0247884754 5" />
-<x-contact-page-card row-class-md="6" row-class-lg="3" logo-class="bi bi-envelope" title="Email Us" description="principal@fec.ac.bd"/>
+<x-contact-page-card row-class-md="6" row-class-lg="6" logo-class="bi bi-geo-alt"
+    title="{{ $main_address['location']['title'] }}" description="{{ $main_address['location']['details'] }}" />
+<x-contact-page-card row-class-md="6" row-class-lg="3" logo-class="bi bi-telephone"
+    title="{{ $main_address['phone']['title'] }}" description="{{ $main_address['phone']['details'] }}" />
+<x-contact-page-card row-class-md="6" row-class-lg="3" logo-class="bi bi-envelope"
+    title="{{ $main_address['email']['title'] }}" description="{{ $main_address['email']['details'] }}" />
