ce20e4218a39b67f3541ddff0ab21e4e5db15792
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-08-13 20:44:35 +0600

    fixed some issues

--- app/Http/Controllers/ContactUsPage/ContactUsPageController.php
+++ app/Http/Controllers/ContactUsPage/ContactUsPageController.php
@@ -10,17 +10,15 @@ class ContactUsPageController extends Controller
     public function load_page()
     {
         $page = get_page_details('contactus');
-        $contact_details = json_decode(PageMetas::where('page_id', $page['id'])->where('meta_key', 'contact_details')->first()->meta_value, true);
-
+        $contact_details = json_decode(
+            PageMetas::where('page_id', $page['id'])->where('meta_key', 'contact_details')->first()->meta_value,
+            true
+        );
         $main_address = json_decode(
             PageMetas::where('page_id', $page['id'])->where('meta_key', 'main_address')->first()->meta_value,
             true
         );
 
-        return view('contact-us', array(
-            'page' => $page,
-            'contact_details' => $contact_details,
-            'main_address' => $main_address
-        ));
+        return view('contact-us', compact('page', 'contact_details', 'main_address'));
     }
 }
