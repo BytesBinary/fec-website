f1f22efd3fc6753c9a409fbd18c7c86027642930
SN Sabit <168827958+Sabit056@users.noreply.github.com>
2024-08-15 17:33:50 +0600

    about college 3 done

--- resources/views/about/vision-mission.blade.php
+++ resources/views/about/vision-mission.blade.php
@@ -1,1 +1,16 @@
 @extends("layouts.app")
+
+@section('content')
+    <div class="container page-top-section">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Vision & Mission | Faridpur Engineering College</h1>
+                <hr>
+                <h2 class="mt-4">ভিশন (Vision)-</h2>
+                <p>প্রকৌশল শিক্ষার মান যুগোপযোগীকরণ, কারিগরি ও দক্ষ মানব সম্পদ উন্নয়ন, অর্থনৈতিক সমৃদ্ধি অর্জন, জীবন যাত্রার মানোন্নয়ন এবং স্মার্ট বাংলাদেশ গঠনে ভূমিকা পালন। </p>
+                <h2 class="mt-4">মিশন (Mission)-</h2>
+                <p> গুণগত মানসম্পন্ন প্রকৌশল শিক্ষা নিশ্চিত করার লক্ষ্যে প্রয়োজনীয় কারিকুলাম ও কর্মসূচি প্রণয়ন, আদর্শ মান নির্ধারণ এবং পরিবীক্ষণ ও মূল্যায়ন।</p>
+            </div>
+        </div>
+    </div>
+@endsection
