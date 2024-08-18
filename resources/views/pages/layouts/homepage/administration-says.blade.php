4ea6c52ec0e58d762271313f15c2d34b5e32e380
Abdullah Al Rafi <rafi.workfile@gmail.com>
2024-08-18 16:15:39 +0600

    added addministration section

--- resources/views/pages/layouts/homepage/administration-says.blade.php
+++ resources/views/pages/layouts/homepage/administration-says.blade.php
@@ -1,4 +1,34 @@
-<h2>
-    Messages from administration
-
-</h2>
+<div class="container professor-intro my-5">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <!-- Professor 1 -->
+        <div class="col">
+            <div class="card h-100 shadow-sm" data-aos="fade-up">
+                <img src="..." class="card-img-top" alt="Professor Dr. Muhammad Samad">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Professor Dr. Muhammad Samad</h5>
+                    <p class="card-text text-muted">Pro Vice Chancellor (Administration)</p>
+                </div>
+            </div>
+        </div>
+        <!-- Professor 2 -->
+        <div class="col">
+            <div class="card h-100 shadow-sm " data-aos="fade-up">
+                <img src="..." class="card-img-top" alt="Professor Dr. Sitesh Chandra Bachar">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Professor Dr. Sitesh Chandra Bachar</h5>
+                    <p class="card-text text-muted">Pro Vice Chancellor (Academic)</p>
+                </div>
+            </div>
+        </div>
+        <!-- Professor 3 -->
+        <div class="col">
+            <div class="card h-100 shadow-sm" data-aos="fade-up">
+                <img src="..." class="card-img-top" alt="Professor Mamtaz Uddin Ahmed">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Professor Mamtaz Uddin Ahmed</h5>
+                    <p class="card-text text-muted">Treasurer</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
