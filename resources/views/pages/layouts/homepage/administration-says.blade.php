2d8dd66cd608b6db59993e2864f4ba7a14ada318
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2024-08-18 16:54:55 +0600

    updated cards

--- resources/views/pages/layouts/homepage/administration-says.blade.php
+++ resources/views/pages/layouts/homepage/administration-says.blade.php
@@ -1,34 +1,53 @@
 <div class="container professor-intro my-5">
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
         <!-- Professor 1 -->
         <div class="col">
-            <div class="card h-100 shadow-sm" data-aos="fade-up">
-                <img src="..." class="card-img-top" alt="Professor Dr. Muhammad Samad">
-                <div class="card-body text-center">
-                    <h5 class="card-title">Professor Dr. Muhammad Samad</h5>
-                    <p class="card-text text-muted">Pro Vice Chancellor (Administration)</p>
+            <a href="#">
+                <div class="card h-100 shadow-lg border-0 rounded-3" data-aos="fade-up">
+                    <img src="{{ asset('assets/img/robin .jpg') }}" class="card-img-top rounded-top" alt="Professor Sheikh Robin">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mb-3 fw-bold">Professor Sheikh Robin</h5>
+                        <p class="card-text text-muted mb-4">Pro Vice Chancellor <br>(Administration)</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
         <!-- Professor 2 -->
-        <div class="col">
-            <div class="card h-100 shadow-sm " data-aos="fade-up">
-                <img src="..." class="card-img-top" alt="Professor Dr. Sitesh Chandra Bachar">
-                <div class="card-body text-center">
-                    <h5 class="card-title">Professor Dr. Sitesh Chandra Bachar</h5>
-                    <p class="card-text text-muted">Pro Vice Chancellor (Academic)</p>
+        <a href="#">
+            <div class="col">
+                <div class="card h-100 shadow-lg border-0 rounded-3" data-aos="fade-up">
+                    <img src="{{ asset('assets/img/jakiur.jpg') }}" class="card-img-top rounded-top" alt="Professor AJDM Jakiur Rahman">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mb-3 fw-bold">Professor AJDM Jakiur Rahman</h5>
+                        <p class="card-text text-muted mb-4">Pro Vice Chancellor(Academic)</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </a>
         <!-- Professor 3 -->
+       <a href="#">
         <div class="col">
-            <div class="card h-100 shadow-sm" data-aos="fade-up">
-                <img src="..." class="card-img-top" alt="Professor Mamtaz Uddin Ahmed">
+            <div class="card h-100 shadow-lg border-0 rounded-3" data-aos="fade-up">
+                <img src="{{ asset('assets/img/rafi.jpg') }}" class="card-img-top rounded-top" alt="Professor MD Abdullah Rafi">
                 <div class="card-body text-center">
-                    <h5 class="card-title">Professor Mamtaz Uddin Ahmed</h5>
-                    <p class="card-text text-muted">Treasurer</p>
+                    <h5 class="card-title mb-3 fw-bold">Professor MD Abdullah Rafi</h5>
+                    <p class="card-text text-muted mb-4">Treasurer</p>
+
                 </div>
             </div>
         </div>
+       </a>
+        <!-- Professor 4 -->
+        <a href="#">
+            <div class="col">
+                <div class="card h-100 shadow-lg border-0 rounded-3" data-aos="fade-up">
+                    <img src="{{ asset('assets/img/sabit.jpg') }}" class="card-img-top rounded-top" alt="Professor MD Abdullah Rafi">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mb-3 fw-bold">Professor SN Sabit</h5>
+                        <p class="card-text text-muted mb-4"><br>Playboy</p>
+                    </div>
+                </div>
+            </div>
+           </a>
     </div>
 </div>
