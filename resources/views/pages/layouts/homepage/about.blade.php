2221bec6e295512e12fd863224d4820eea403262
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-08-21 20:55:59 +0600

    write some css code

--- resources/views/pages/layouts/homepage/about.blade.php
+++ resources/views/pages/layouts/homepage/about.blade.php
@@ -1,8 +1,8 @@
-<section id="features-details" class="features-details section">
+<section class="about-section section">
     <div class="container">
         <x-section-title title="About Us" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-6 text">
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
@@ -14,6 +14,11 @@
                     Where does it come from?
                     Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum
                 </p>
+                <a href="{{route('about-college/at-a-glance')}}">
+                    <div class="d-grid">
+                        <button class="btn btn-warning" type="button">Read More <span><i class="bi bi-box-arrow-in-right"></i></span></button>
+                    </div>
+                </a>
             </div>
             <div class="col-sm-6">
                 <img src="{{ asset('assets/img/mia-bhai.jpg') }}" class="img-fluid float-left mr-4 mb-4" alt="Image Description"/>
