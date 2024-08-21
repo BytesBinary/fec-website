80072b0e84f44b66053069b57340437ede4b3507
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-08-21 23:40:22 +0600

    padding remove from about section

--- resources/views/pages/layouts/homepage/about.blade.php
+++ resources/views/pages/layouts/homepage/about.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <x-section-title title="About Us" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." animation="fade-up" />
         <div class="row">
-            <div class="col-sm-6 text px-5" data-aos="fade-up-right">
+            <div class="col-sm-6 text" data-aos="fade-up-right">
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
@@ -16,7 +16,7 @@
                     </div>
                 </a>
             </div>
-            <div class="col-sm-6 position-relative img-side rounded px-3" data-aos="fade-up-left">
+            <div class="col-sm-6 img-side rounded px-3" data-aos="fade-up-left">
                 <div class="play-button-container rounded-circle position-absolute top-50 start-50 translate-middle d-flex justify-content-center align-items-center bg-warning pulse" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
                     <a href="https://www.youtube.com/embed/_H9B4iynTY0?si=hssoHPSX4b3isoSt" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-caret-right-fill text-light"></i></a>
                 </div>
