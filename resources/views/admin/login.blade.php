2c0a63ece39f0ffff0578a8389341b95384da343
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-08-23 00:02:50 +0600

    cerate admin login page

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Purple Admin</title>
+    @include('admin.layouts.partials.styles')
+</head>
+<body>
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth">
+            <div class="row flex-grow">
+                <div class="col-lg-4 mx-auto">
+                    <div class="auth-form-light text-left p-5">
+                        <div class="brand-logo text-center">
+                            <img src="{{asset('assets/img/FEClogo.png')}}" class="img-fluid text-center">
+                        </div>
+                        <h2 class="text-center">Admin Login</h2>
+                        <form class="pt-3">
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+                            <div class="mt-3 d-grid gap-2">
+                                <a class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">SIGN IN</a>
+                            </div>
+                            <div class="my-2 d-flex justify-content-between align-items-center">
+                                <a href="#" class="auth-link text-primary">Forgot password?</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@include('admin.layouts.partials.scripts')
+</body>
+</html>
