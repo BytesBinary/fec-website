f2f246995e20dbb1e74d0a9537b2bfc09f7952a8
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2024-08-28 00:08:46 +0600

    updated,controller,seeder,about,administration,short-details

--- resources/views/pages/partials/home/administration-section.blade.php
+++ resources/views/pages/partials/home/administration-section.blade.php
@@ -6,13 +6,13 @@
         <div class="row g-4">
             @foreach ( $administration['members'] as $key => $member )
             <x-card
-            row-class="col-sm-4"
+            row-class="col-sm-3"
             link="#"
             animation="{{$key === 0 ? 'fade-right' : ($key === 1 ? 'fade-up' : ($key === 2 ? 'fade-down' : 'fade-left')) }}"
             image="true"
             image-class="rounded-top"
             cardimage="true"
-            imageUrl="{{ asset('assets/img/robin .jpg') }}"
+            imageUrl="{{ $member['img'] }}"
             imageAlt="{{ $member['name'] }}"
             body-class="text-center" cardTitle="{{ $member['name'] }}" title-class="mb-3 fw-bold"
             cardText="{{ $member['designation'] }}"
