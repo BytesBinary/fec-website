49212535e2233735be4a43ab9ef3262e321cfcc2
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2024-08-31 20:09:21 +0600

    Updated modal details for home page

--- resources/views/admin/edit-pages/edit-home.blade.php
+++ resources/views/admin/edit-pages/edit-home.blade.php
@@ -25,20 +25,53 @@
             </x-forms>
         </x-modal>
 
-        <x-modal modal-id="administration_section" modal-title="Administration" modal-class="modal-fullscreen">
-            Content for Administration section.
+        <x-modal modal-id="administration_section" modal-title="Administration">
+            <x-forms action="" method="POST" >
+                <div class="row">
+                    <h2 class="text-center"> Edit Administration Section </h2>
+                    <x-form-input name="Name" label="Name" placeholder="Enter your name" container-class="col-sm-12" />
+                    <x-form-input name="designation" label="Name" placeholder="Designation" container-class="col-sm-12" />
+                    <x-form-input type="file" name="image" label="Image" container-class="col-sm-12" />
+                    <x-form-input type="button" name="submit" value="Submit" container-class="col-sm-12" class="btn btn-primary" />
+                </div>
+            </x-forms>
         </x-modal>
 
-        <x-modal modal-id="short_details_section" modal-title="Short Details" modal-class="modal-fullscreen">
-            Content for Short Details section.
+        <x-modal modal-id="short_details_section" modal-title="Short Details">
+            <x-forms action="" method="POST" >
+                <div class="row">
+                    <h2 class="text-center"> Edit Short Details Section </h2>
+                    <x-form-input name="title" label="Title" placeholder="Enter The Title" container-class="col-sm-12" />
+                    <x-form-input name="Value" label="Total Value" placeholder="Amount" container-class="col-sm-12" />
+                    <x-form-input type="file" name="image" label="Image" container-class="col-sm-12" />
+                    <x-form-input type="button" name="submit" value="Submit" container-class="col-sm-12" class="btn btn-primary" />
+                </div>
+            </x-forms>
         </x-modal>
 
-        <x-modal modal-id="online_services_section" modal-title="Online Services" modal-class="modal-fullscreen">
-            Content for Online Services section.
+        <x-modal modal-id="online_services_section" modal-title="Online Services">
+            <x-forms action="" method="POST" >
+                <div class="row">
+                    <h2 class="text-center"> Edit Short Online Services Section </h2>
+                    <x-form-input name="title" label="Title" placeholder="Enter The Title" container-class="col-sm-12" />
+                    <x-form-input name="description" label="Description" placeholder="Description" container-class="col-sm-12" />
+                    <x-form-input name="btn-url" label="Button URL" placeholder="Button URL" container-class="col-sm-12" />
+                    <x-form-input name="btn-txt" label="Button Text" placeholder="Button Text" container-class="col-sm-12" />
+                    <x-form-input type="file" name="image" label="Image" container-class="col-sm-12" />
+                    <x-form-input type="button" name="submit" value="Submit" container-class="col-sm-12" class="btn btn-primary" />
+                </div>
+            </x-forms>
         </x-modal>
 
-        <x-modal modal-id="faq_section" modal-title="FAQ" modal-class="modal-fullscreen">
-            Content for FAQ section.
+        <x-modal modal-id="faq_section" modal-title="FAQ" >
+            <x-forms action="" method="POST" >
+                <div class="row">
+                    <h2 class="text-center"> Edit FAQ</h2>
+                    <x-form-input name="title" label="Question Title" placeholder="Feel free to ask" container-class="col-sm-12" />
+                    <x-form-input name="description" label="Description" placeholder="Question Description" container-class="col-sm-12" />
+                    <x-form-input type="button" name="submit" value="Submit" container-class="col-sm-12" class="btn btn-primary" />
+                </div>
+            </x-forms>
         </x-modal>
     </div>
 @endsection
