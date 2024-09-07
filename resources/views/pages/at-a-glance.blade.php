55f57a9964bb7c65fa3c9be03a58cd112382c54d
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-09-08 04:26:42 +0600

    Update at a glance

--- resources/views/pages/at-a-glance.blade.php
+++ resources/views/pages/at-a-glance.blade.php
@@ -4,26 +4,28 @@
 
 @section('content')
 <div class="container page-top-section">
-    <div class="row clearfix">
-        <x-section-title title="At a Glance" des="" animation="fade-up" />
-        <div class="row justify-content-center align-items-center">
-            <div class="col-sm-6 img-side rounded px-3 mobile-height d-flex align-items-center" data-aos="fade-up-left">
-                <div class="position-relative w-100">
-                    <img src="{{asset('images/'.$section->image)}}" class="img-fluid rounded px-3 mobile-height" alt="FEC Image" style="object-fit: cover;">
-                    <div class="play-button-container rounded-circle position-absolute top-50 start-50 translate-middle d-flex justify-content-center align-items-center bg-warning pulse" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
-                        <a href="{{$section->video_url}}" class="glightbox btn-watch-video d-flex align-items-center">
-                            <i class="bi bi-caret-right-fill text-light"></i>
-                        </a>
+    <div class="row">
+        @if(!empty($section))
+            <x-section-title title="At a Glance" des="" animation="fade-up" />
+            <div class="row justify-content-center align-items-center">
+                <div class="col-sm-12 img-side rounded px-3 mobile-height d-flex align-items-center" data-aos="fade-up-left">
+                    <div class="position-relative w-100">
+                        <img src="{{asset('images/'.$section->image)}}" class="img-fluid rounded px-3 mobile-height" alt="FEC Image" style="object-fit: cover;">
+                        <div class="play-button-container rounded-circle position-absolute top-50 start-50 translate-middle d-flex justify-content-center align-items-center bg-warning pulse" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+                            <a href="{{$section->video_url}}" class="glightbox btn-watch-video d-flex align-items-center">
+                                <i class="bi bi-caret-right-fill text-light"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-sm-12" data-aos="fade-up-right">
-            <p style="text-align: justify">
-               {!! nl2br(e($section->content))  !!}
-            </p>
-        </div>
+            <div class="col-sm-12" data-aos="fade-up-right">
+                <p style="text-align: justify">
+                    {!! nl2br(e($section->content))  !!}
+                </p>
+            </div>
+        @endif
     </div>
 </div>
 
