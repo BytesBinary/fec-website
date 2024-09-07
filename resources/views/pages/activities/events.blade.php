eec5f84d514c62eb847f61bba450e4a0b5827e68
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-09-08 04:52:45 +0600

    Add event date

--- resources/views/pages/activities/events.blade.php
+++ resources/views/pages/activities/events.blade.php
@@ -12,7 +12,7 @@
                 <x-card row-class="col-lg-4 col-md-6 mb-4" card-class="h-100 shadow-lg border-0" image="true"
                     image-url="{{ asset($event->cover) }}" image-alt="Features 3"
                     image-class="card-img-top img-fluid"
-                    custom-html-in-card='<hr class="custom-line"><p class="text-muted mb-2 date"><i class="bi bi-calendar"></i> 21 Aug, 2024</p><hr class="custom-line">'
+                    custom-html-in-card='<hr class="custom-line"><p class="text-muted mb-2 date"><i class="bi bi-calendar"></i> {{$event->date}}</p><hr class="custom-line">'
                     card-title="{{ $event->title }}" title-class="fw-bold mb-3"
                     card-text="{{ Str::limit(strip_tags($event->content), 100) }}"
                     cardlink="true" card-link-url="{{ route('event.view',['slug'=>$event->slug]) }}" card-link-class="btn btn-outline-primary rounded-pill"
