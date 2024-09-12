64cab375ad4a827ad29cf83b6895607eeee32057
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-09-12 16:58:16 +0600

    done the key-features

--- resources/views/partials/landing/administration.blade.php
+++ resources/views/partials/landing/administration.blade.php
@@ -1,4 +1,4 @@
-<div class="container mx-auto shadow-2xl shadow-gray-100 bg-white p-5 my-10 rounded-lg shadow-inner">
+<div class="container mx-auto bg-white p-5 my-10 rounded-lg shadow-inner">
     <p class="text-center mt-10 mb-8 text-5xl font-bold text-gray-950">Administration</p>
     <hr class="w-48 h-1 mx-auto bg-gray-400 border-0 rounded dark:bg-gray-700">
     <div class="grid gap-5 grid-cols-1 mt-5 lg:grid-cols-4">
