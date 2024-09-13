e037250a143ce3717c10a81d5f4933f451b8de4a
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-09-13 14:39:45 +0600

    Update administration.blade.php

--- resources/views/partials/landing/administration.blade.php
+++ resources/views/partials/landing/administration.blade.php
@@ -3,7 +3,7 @@
     <hr class="w-48 h-1 mx-auto bg-gray-400 border-0 rounded dark:bg-gray-700">
     <div class="grid gap-5 grid-cols-1 mt-5 lg:grid-cols-4">
         <x-administration-card name="Prof. Dr. Engr. Md. Mizanur Rahman" designation="Principal" image="images/administration/mia-bhai.jpg" url="{{route('principal')}}" animation="fade-up" />       
-        <div class="md:col-span-3 card w-full shadow-2xl shadow-gray-500 mt-5 hover:shadow-gray-700 hover:cursor-pointer" data-aos="fade-up">
+        <div class="lg:col-span-3 card w-full shadow-2xl shadow-gray-500 mt-5 hover:shadow-gray-700 hover:cursor-pointer" data-aos="fade-up">
             <div class="card-body">
                 <h1 class="text-2xl">Message From Principal</h1>
                 <p class="mt-3 md:text-lg text-gray-950 text-justify">
