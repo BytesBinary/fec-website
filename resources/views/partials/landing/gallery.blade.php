26233e1c8ca5a1fab9c53e2bcec3c082e2361006
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2024-09-26 22:29:35 +0600

    fixed minor issue

--- resources/views/partials/landing/gallery.blade.php
+++ resources/views/partials/landing/gallery.blade.php
@@ -1,10 +1,4 @@
-<div class="mx-0 grid grid-cols-5">
-    <div class="">
-        <figure class="overflow-hidden">
-            <img src="{{ asset('images/card/cse-dept.jpg') }}" alt="car"
-                class="w-full h-96 object-cover hover:scale-105 transition-transform duration-300" />
-        </figure>
-    </div>
+<div class="mx-0 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 ">
     <div class="">
         <figure class="overflow-hidden">
             <img src="{{ asset('images/card/cse-dept.jpg') }}" alt="car"
