5e5d148619ca773d7072fa6c2cdd75bf0cafbf03
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2024-09-26 22:06:42 +0600

    Notice box updated

--- resources/views/partials/landing/dept-and-notice.blade.php
+++ resources/views/partials/landing/dept-and-notice.blade.php
@@ -8,50 +8,88 @@
             <div class="swiper-container">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
-                        <x-departments-card name="Computer Science & Engineering" image="images/card/cse-dept.jpg" url="#" is-home-page="true" />
+                        <x-departments-card name="Computer Science & Engineering" image="images/card/cse-dept.jpg"
+                            url="#" is-home-page="true" />
                     </div>
                     <div class="swiper-slide">
-                        <x-departments-card name="Electrical & Electronics Engineering" image="images/card/cse-dept.jpg" url="#" is-home-page="true" />
+                        <x-departments-card name="Electrical & Electronics Engineering" image="images/card/cse-dept.jpg"
+                            url="#" is-home-page="true" />
                     </div>
                     <div class="swiper-slide">
-                        <x-departments-card name="Civil <br> Engineering" image="images/card/cse-dept.jpg" url="#" is-home-page="true" />
+                        <x-departments-card name="Civil <br> Engineering" image="images/card/cse-dept.jpg"
+                            url="#" is-home-page="true" />
                     </div>
                     <div class="swiper-slide">
-                        <x-departments-card name="Computer Science & Engineering" image="images/card/cse-dept.jpg" url="#" is-home-page="true" />
+                        <x-departments-card name="Computer Science & Engineering" image="images/card/cse-dept.jpg"
+                            url="#" is-home-page="true" />
                     </div>
                     <div class="swiper-slide">
-                        <x-departments-card name="Electrical & Electronics Engineering" image="images/card/cse-dept.jpg" url="#" is-home-page="true" />
+                        <x-departments-card name="Electrical & Electronics Engineering" image="images/card/cse-dept.jpg"
+                            url="#" is-home-page="true" />
                     </div>
                     <div class="swiper-slide">
-                        <x-departments-card name="Civil <br> Engineering" image="images/card/cse-dept.jpg" url="#" is-home-page="true" />
+                        <x-departments-card name="Civil <br> Engineering" image="images/card/cse-dept.jpg"
+                            url="#" is-home-page="true" />
                     </div>
                 </div>
                 <div class="swiper-pagination"></div>
             </div>
         </div>
 
-        <div class="w-full col-span-1 flex flex-col">
+        <div class="w-full col-span-1">
             <p class="text-center mb-8 text-3xl md:text-5xl font-bold " data-aos="fade-up">
                 Notices
             </p>
-            <hr class="w-48 h-1 mx-auto bg-green-400 border-0 rounded mb-10" data-aos="fade-down">
-            <div class="relative bg-gray-100 rounded-lg overflow-hidden flex-1">
+            <hr class="w-48 h-1 mx-auto bg-green-400 border-0 rounded mb-10" data-aos="fade-down" />
+            <div class="relative bg-gray-100 rounded-lg overflow-hidden h-[510px]">
                 <!-- Notice Container -->
                 <div class="overflow-hidden relative h-full">
                     <div class="notice-wrapper space-y-4 animate-scroll p-3">
-                        <div class="notice py-2">Notice 1: New event coming up!</div>
-                        <div class="notice py-2">Notice 2: Meeting tomorrow at 10 AM</div>
-                        <div class="notice py-2">Notice 3: Assignment submission extended</div>
-                        <div class="notice py-2">Notice 4: Holidays announced for next week</div>
-                        <div class="notice py-2">Notice 5: New project deadline is next Monday</div>
-                        <div class="notice py-2">Notice 6: Team meeting at 3 PM today</div>
-                        <div class="notice py-2">Notice 7: New library timings from next week</div>
-                        <div class="notice py-2">Notice 8: Graduation ceremony dates announced</div>
-                        <div class="notice py-2">Notice 9: Sports event registration opens tomorrow</div>
-                        <div class="notice py-2">Notice 10: Office hours updated for next week</div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 1: New event coming up!</a>
+                        </div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 2: Meeting tomorrow at 10 AM</a>
+                        </div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 3: Assignment submission extended</a>
+                        </div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 4: Holidays announced for next
+                                week</a>
+                        </div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 5: New project deadline is next
+                                Monday</a>
+                        </div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 6: Team meeting at 3 PM today</a>
+                        </div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 7: New library timings from next
+                                week</a>
+                        </div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 8: Graduation ceremony dates
+                                announced</a>
+                        </div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 9: Sports event registration opens
+                                tomorrow</a>
+                        </div>
+                        <div class="notice py-2 mx-2 text-lg bg-white border px-3 rounded-lg shadow-lg">
+                            <a class="hover:text-blue-500" href=""> Notice 10: Office hours updated for next
+                                week</a>
+                        </div>
                     </div>
                 </div>
+
+                <div class="absolute bottom-4 left-1/2 w-[91%] text-center transform -translate-x-1/2 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-semibold py-3 px-4 rounded-lg shadow-md hover:shadow-lg hover:from-blue-600 hover:to-blue-800 transition duration-300 ease-in-out">
+                    <a href="#">View more</a>
+                </div>
             </div>
+
         </div>
+
     </div>
 </div>
