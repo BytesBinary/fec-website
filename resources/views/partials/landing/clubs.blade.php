df2b6bb91f81ba3ece4046af8436c9c53ba3f85f
SN Sabit <shiblinomani90@gmail.com>
2024-09-27 17:34:13 +0600

    photos and name has been updated

--- resources/views/partials/landing/clubs.blade.php
+++ resources/views/partials/landing/clubs.blade.php
@@ -4,39 +4,43 @@
     <div class="swiper clubs-page-swiper mt-10">
         <div class="swiper-wrapper mb-10">
             <div class="swiper-slide">
-                <x-club-card logo="images/BytesBinary.png" short-name="FECPC" full-name="Fardipur Engineering College Programming Club"
+                <x-club-card logo="{{asset('images/club/ric.jpg')}}" short-name="RIC" full-name="Research and Innovation Center"
+                             description="At RIC FEC, we cultivate a culture of innovation, motivation, and leadership among engineers and aspiring researchers." />
+            </div>
+            <div class="swiper-slide">
+                <x-club-card logo="images/BytesBinary.png" short-name="FECCPC" full-name="Faridpur Engineering College Cyber Protector Club"
                              description="This is a honourable club of faridpur engineering college. Please explore it by clicking the explore button." />
             </div>
             <div class="swiper-slide">
-                <x-club-card logo="images/BytesBinary.png" short-name="FECPC" full-name="Fardipur Engineering College Programming Club"
+                <x-club-card logo="{{asset('images/club/fecsa.jpg')}}" short-name="FECSA" full-name="Faridpur Engineering College Sports Association"
                              description="This is a honourable club of faridpur engineering college. Please explore it by clicking the explore button." />
             </div>
             <div class="swiper-slide">
-                <x-club-card logo="images/BytesBinary.png" short-name="FECPC" full-name="Fardipur Engineering College Programming Club"
+                <x-club-card logo="{{asset('images/club/photography.jpg')}}" short-name="FECPC" full-name="Faridpur Engineering College Photographic Club"
                              description="This is a honourable club of faridpur engineering college. Please explore it by clicking the explore button." />
             </div>
             <div class="swiper-slide">
-                <x-club-card logo="images/BytesBinary.png" short-name="FECPC" full-name="Fardipur Engineering College Programming Club"
+                <x-club-card logo="{{asset('images/club/programming.jpg')}}" short-name="FECPC" full-name="Faridpur Engineering College Programming Club"
                              description="This is a honourable club of faridpur engineering college. Please explore it by clicking the explore button." />
             </div>
             <div class="swiper-slide">
-                <x-club-card logo="images/BytesBinary.png" short-name="FECPC" full-name="Fardipur Engineering College Programming Club"
+                <x-club-card logo="images/BytesBinary.png" short-name="FECSDC" full-name="Faridpur Engineering College Software Development Club"
                              description="This is a honourable club of faridpur engineering college. Please explore it by clicking the explore button." />
             </div>
             <div class="swiper-slide">
-                <x-club-card logo="images/BytesBinary.png" short-name="FECPC" full-name="Fardipur Engineering College Programming Club"
+                <x-club-card logo="{{asset('images/club/fecrdc.jpg')}}" short-name="FECIDRC" full-name="Faridpur Engineering College Islamic Dawah and Research Center"
                              description="This is a honourable club of faridpur engineering college. Please explore it by clicking the explore button." />
             </div>
             <div class="swiper-slide">
-                <x-club-card logo="images/BytesBinary.png" short-name="FECPC" full-name="Fardipur Engineering College Programming Club"
+                <x-club-card logo="{{asset('images/club/professionals.jpg')}}" short-name="FECCP" full-name="Faridpur Engineering College Club of Professionals"
                              description="This is a honourable club of faridpur engineering college. Please explore it by clicking the explore button." />
             </div>
             <div class="swiper-slide">
-                <x-club-card logo="images/BytesBinary.png" short-name="FECPC" full-name="Fardipur Engineering College Programming Club"
+                <x-club-card logo="images/BytesBinary.png" short-name="FECBAC" full-name="Faridpur Engineering College Bani Archana Committee"
                              description="This is a honourable club of faridpur engineering college. Please explore it by clicking the explore button." />
             </div>
             <div class="swiper-slide">
-                <x-club-card logo="images/BytesBinary.png" short-name="FECPC" full-name="Fardipur Engineering College Programming Club"
+                <x-club-card logo="images/BytesBinary.png" short-name="FECRSG" full-name="Faridpur Engineering College Rover Scout Group"
                              description="This is a honourable club of faridpur engineering college. Please explore it by clicking the explore button." />
             </div>
         </div>
