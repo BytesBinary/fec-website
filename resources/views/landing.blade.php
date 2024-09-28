f2a91a130e3cd36ec59be5991501f70369bd1cb9
Abdullah Al Rafi <rafi.workfile@gmail.com>
2024-09-28 21:43:25 +0600

    Research-Events-News

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,6 +8,7 @@
     @include('partials.landing.dept-and-notice')
     @include('partials.landing.short-details')
     @include('partials.landing.about')
+    @include('partials.landing.research-events-news')
     {{-- @include('partials.landing.alumni') --}}
     @include('partials.landing.fec-website-linking')
     @include('partials.landing.gallery')
