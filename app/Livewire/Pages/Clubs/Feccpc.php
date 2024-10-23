6bfa52878d319ddde93904eaf70574f2c1d67469
SN Sabit <shiblinomani90@gmail.com>
2024-10-23 13:08:32 +0600

    club-page and route setting have finished

---
+++ app/Livewire/Pages/Clubs/Feccpc.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Pages\Clubs;
+
+use Livewire\Component;
+
+class Feccpc extends Component
+{
+    public $club='protector club';
+    public function render()
+    {
+        return view('livewire.pages.campus-life/club');
+    }
+}
