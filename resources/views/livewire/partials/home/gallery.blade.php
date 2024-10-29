971e17fa158659e48830fe309467736bd1abb207
SN Sabit <shiblinomani90@gmail.com>
2024-10-29 21:50:44 +0600

    fixed the asset issue

--- resources/views/livewire/partials/home/gallery.blade.php
+++ resources/views/livewire/partials/home/gallery.blade.php
@@ -5,8 +5,9 @@
         <div class="swiper-slide">
             <div class="relative">
                 <figure class="overflow-hidden">
-                    <a href="{{ $gallery['src'] }}" class="glightbox" data-gallery="gallery1">
-                        <img src="{{ $gallery['src'] }}" alt="Gallery Image"
+                    <a href="{{ asset($gallery['src']) }}">
+                        class="glightbox" data-gallery="gallery1">
+                        <img src="{{ asset($gallery['src']) }}" alt="Gallery Image"
                              class="w-full h-64 sm:h-72 md:h-80 lg:h-96 object-cover transition-transform duration-300 hover:scale-105 hover:cursor-zoom-in" />
                         <div class="overlay absolute inset-0 bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 hover:opacity-100 hover:cursor-zoom-in"></div>
                     </a>
