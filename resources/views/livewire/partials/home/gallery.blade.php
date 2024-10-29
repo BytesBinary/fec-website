0f6d27ad5a80756a19f671913686fb2031e28b72
SN Sabit <shiblinomani90@gmail.com>
2024-10-29 19:30:22 +0600

    gallery dynamic done

--- resources/views/livewire/partials/home/gallery.blade.php
+++ resources/views/livewire/partials/home/gallery.blade.php
@@ -1,17 +1,23 @@
 <div class="swiper-container swiper-container-gallery py-10">
     <div class="swiper-wrapper mb-10">
+
+        @foreach ($Galleries as $gallery)
         <div class="swiper-slide">
             <div class="relative">
                 <figure class="overflow-hidden">
-                    <a href="{{ asset('images/gallery/1.jpg') }}" class="glightbox" data-gallery="gallery1">
-                        <img src="{{ asset('images/gallery/1.jpg') }}" alt="Gallery Image"
+                    <a href="{{ $gallery['src'] }}" class="glightbox" data-gallery="gallery1">
+                        <img src="{{ $gallery['src'] }}" alt="Gallery Image"
                              class="w-full h-64 sm:h-72 md:h-80 lg:h-96 object-cover transition-transform duration-300 hover:scale-105 hover:cursor-zoom-in" />
                         <div class="overlay absolute inset-0 bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 hover:opacity-100 hover:cursor-zoom-in"></div>
                     </a>
                 </figure>
             </div>
-        </div>
-        <div class="swiper-slide">
+            </div>
+        @endforeach
+
+
+
+        <!--<div class="swiper-slide">
             <div class="relative">
                 <figure class="overflow-hidden">
                     <a href="{{ asset('images/gallery/2.jpg') }}" class="glightbox" data-gallery="gallery1">
@@ -109,7 +115,7 @@
                     </a>
                 </figure>
             </div>
-        </div>
+        </div>--->
     </div>
 </div>
 @push('scripts')
