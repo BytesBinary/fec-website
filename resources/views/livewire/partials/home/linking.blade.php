e30162e245efc5ec014a3c3799492a257ef3894f
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2024-11-01 17:17:23 +0600

    fixed footer

--- resources/views/livewire/partials/home/linking.blade.php
+++ resources/views/livewire/partials/home/linking.blade.php
@@ -1,4 +1,4 @@
-<section class="container mx-auto p-8 bangla-font">
+<section class="container mx-auto p-8 bangla-font" id="linking">
     <h1 class="text-center text-5xl mb-5" >অনলাইন সেবা সমূহ</h1>
     <hr class="w-48 h-1 mx-auto bg-gray-400 border-0 rounded" >
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-5">
