ea0831739cbdbec4fe641f324ea755ab5496722d
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2024-11-02 01:02:46 +0600

    Added button

--- resources/views/components/card/research-card.blade.php
+++ resources/views/components/card/research-card.blade.php
@@ -1,14 +1,14 @@
 @props([
-    'image' => '',
+    'image' => 'images/research/research.png',
     'title' => '',
     'description' => '',
     'date'=>'',
     'url' => '#',
 ])
 
-<div class="bg-white rounded-lg shadow-lg overflow-hidden transform transition-all duration-300 hover:shadow-2xl hover:scale-105">
+<div class="h-[26rem] bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:shadow-2xl">
     <img src="{{ asset($image) }}" alt="Image"
-        class="w-full h-56 object-cover transition-opacity duration-300 hover:opacity-90">
+        class="w-full h-56 object-cover">
     <div class="p-6 space-y-4">
         <!-- Event Title -->
         <h3 class="text-2xl font-semibold text-gray-800 transition-colors duration-300 hover:text-green-600">
--- resources/views/livewire/partials/home/research-events.blade.php
+++ resources/views/livewire/partials/home/research-events.blade.php
@@ -9,18 +9,24 @@
             <div class="swiper-wrapper mb-10">
                 @foreach ($events as $event)
                     <div class="swiper-slide">
-                        <x-card.research-card
-                            image="{{ $event['image'] }}"
-                            title="{{ $event['title'] }}"
-                            description="{{ $event['description'] }}"
-                            date="{{ $event['date'] }}"
-                            url="{{ $event['url'] }}"
-                        />
+                        <x-card.research-card image="{{ $event['image'] }}" title="{{ $event['title'] }}"
+                            description="{{ $event['description'] }}" date="{{ $event['date'] }}"
+                            url="{{ $event['url'] }}" />
                     </div>
                 @endforeach
             </div>
             <div class="swiper-pagination"></div>
         </div>
+
+        <div class="card-actions mt-8 mb-4 flex justify-center">
+            <button href='{{ route('events') }}' type="button"
+                class="w-full max-w-xs bg-gradient-to-r from-lime-300 to-emerald-400 text-white hover:bg-gradient-to-l hover:from-lime-300 hover:to-emerald-400 rounded-lg py-4 transition-all flex items-center justify-center space-x-2 shadow-md hover:shadow-lg hover:cursor-pointer"
+                wire:navigate>
+                <span class="text-gray-900 text-xl">View More</span>
+                <x-svg-icon.external-url />
+            </button>
+        </div>
+
     </section>
 
     <section class="mt-5 mb-5">
@@ -33,18 +39,24 @@
             <div class="swiper-wrapper mb-10">
                 @foreach ($publications as $publication)
                     <div class="swiper-slide">
-                        <x-card.research-card
-                            image="{{ $publication['image'] }}"
-                            title="{{ $publication['title'] }}"
-                            description="{{ $publication['description'] }}"
-                            date="{{ $publication['date'] }}"
-                            url="{{ $publication['url'] }}"
-                        />
+                        <x-card.research-card image="{{ $publication['image'] }}" title="{{ $publication['title'] }}"
+                            description="{{ $publication['description'] }}" date="{{ $publication['date'] }}"
+                            url="{{ $publication['url'] }}" />
                     </div>
                 @endforeach
             </div>
             <div class="swiper-pagination"></div>
         </div>
+
+        <div class="card-actions mt-8 mb-4 flex justify-center">
+            <button href='{{ route('publications') }}' type="button"
+                class="w-full max-w-xs bg-gradient-to-r from-lime-300 to-emerald-400 text-white hover:bg-gradient-to-l hover:from-lime-300 hover:to-emerald-400 rounded-lg py-4 transition-all flex items-center justify-center space-x-2 shadow-md hover:shadow-lg hover:cursor-pointer"
+                wire:navigate>
+                <span class="text-gray-900 text-xl">View More</span>
+                <x-svg-icon.external-url />
+            </button>
+        </div>
+
     </section>
 </div>
 
