5ea850642400e8ff0a738ccfa60babb7a9bcebf4
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-11-02 16:52:45 +0600

    short the condition for better performance

--- app/Helper.php
+++ app/Helper.php
@@ -123,8 +123,8 @@ if( ! function_exists('get_sub_page_layout_data') ) {
 
 if( ! function_exists('get_card_class_of_student_staticstics') ) {
     function get_card_class_of_student_staticstics ( $index ) {
-        $class =  ($index/4) & 1 ? ($index % 2 == 0 ? 'bg-lime-50' : 'bg-green-100') : ($index % 2 != 0 ? 'bg-lime-50' : 'bg-green-100');
+        $class = ((($index / 4) % 2 === 0) === ($index % 2 === 0)) ? 'bg-lime-50' : 'bg-white';
         $default = 'shadow-lg rounded-lg p-6 text-center transform hover:scale-105 transition duration-300';
-        return $class . ' ' . $default;
+        return "$class $default";
     }
 }
