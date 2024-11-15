a04e9c015f8fd877db5a0bdaf3d6e17f46b2ed90
light <145149707+shrobin13@users.noreply.github.com>
2024-11-16 00:54:00 +0600

    removed param

--- routes/activities.php
+++ routes/activities.php
@@ -9,5 +9,5 @@ Route::prefix('activities')->group(function (){
     Route::get('/publications', Publications::class)->name('publications');
     Route::get('/researches', Researches::class)->name('researches');
     Route::get('/events', Events::class)->name('events');
-    Route::get('/event/{eventId}',EventPage::class)->name('event');
+    Route::get('/event',EventPage::class)->name('event');
 });
