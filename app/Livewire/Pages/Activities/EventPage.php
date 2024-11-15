f2370ce182c0f4c38950588c3dc0cc26d7c0e075
light <145149707+shrobin13@users.noreply.github.com>
2024-11-16 00:50:18 +0600

    added eventPage

---
+++ app/Livewire/Pages/Activities/EventPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Activities;
+
+use Livewire\Component;
+
+class EventPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.activities.event-page');
+    }
+}
