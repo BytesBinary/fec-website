6b5e59ae01b81ab3baa0a330fb38a279f1dd32f2
Abdullah Al Rafi <rafi.workfile@gmail.com>
2024-11-17 22:37:14 +0600

    a new design theme

--- resources/views/livewire/pages/academics/student-statistics.blade.php
+++ resources/views/livewire/pages/academics/student-statistics.blade.php
@@ -11,5 +11,39 @@
             </div>
           @endforeach
         </div>
+
+        <div class="flex flex-wrap gap-6 mt-6">
+            <div class="relative flex items-center p-4 border-4 border-blue-300 rounded-lg bg-blue-50 shadow-md overflow-hidden w-full md:w-[calc(33%-1rem)]">
+              <div class="w-2/3 z-10">
+                <h2 class="text-2xl font-bold text-gray-800">Dynamic Background</h2>
+                <p class="mt-2 text-sm text-gray-600">Now Style this!!</p>
+              </div>
+              <div class="absolute -right-20 w-1/2 h-full bg-blue-300 rounded-full overflow-hidden">
+                <img src="{{asset('images/event/books.png')}}" alt="Books" class=" w-full h-full p-2" />
+              </div>
+            </div>
+            <div class="relative flex items-center p-4 border-4 border-blue-300 rounded-lg bg-blue-50 shadow-md overflow-hidden w-full md:w-[calc(33%-1rem)]">
+                <div class="w-2/3 z-10">
+                  <h2 class="text-2xl font-bold text-gray-800">Dynamic Background</h2>
+                  <p class="mt-2 text-sm text-gray-600">Now Style this Saniad!!! fksjd fksdfsjd flks df jlksdfj</p>
+                </div>
+                <div class="absolute -right-20 w-1/2 h-full bg-blue-300 rounded-full overflow-hidden">
+                  <img src="{{asset('images/event/books.png')}}" alt="Books" class=" w-full h-full p-2" />
+                </div>
+              </div>
+              <div class="relative flex items-center p-4 border-4 border-blue-300 rounded-lg bg-blue-50 shadow-md overflow-hidden w-full md:w-[calc(33%-1rem)]">
+                <div class="w-2/3 z-10">
+                  <h2 class="text-2xl font-bold text-gray-800">Dynamic Background</h2>
+                  <p class="mt-2 text-sm text-gray-600">Now Style this Saniad!!! fksjd fksdfsjd flks df jlksdfj</p>
+                </div>
+                <div class="absolute -right-20 w-1/2 h-full bg-blue-300 rounded-full overflow-hidden">
+                  <img src="{{asset('images/event/cap.png')}}" alt="Books" class=" w-full h-full p-2" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
     </div>
 </div>
