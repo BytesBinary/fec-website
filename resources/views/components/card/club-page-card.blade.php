84ce6e9cce9ed9c0ea33fbd050925568b475f225
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2024-11-29 01:56:06 +0600

    cub done

--- resources/views/components/card/club-page-card.blade.php
+++ resources/views/components/card/club-page-card.blade.php
@@ -7,25 +7,25 @@
 
 
 
-<div class="flex flex-col w-full max-w-4xl mx-auto my-6 items-center bg-gradient-to-br from-white via-gray-100 to-gray-50 shadow-lg rounded-lg hover:cursor-pointer md:flex-row relative overflow-hidden">
+<div class="flex flex-col w-full max-w-4xl mx-auto my-6 items-center bg-gradient-to-br from-blue-100 via-emerald-50 to-blue-100 shadow-lg rounded-lg hover:cursor-pointer md:flex-row relative overflow-hidden">
     <!-- Image -->
-    <img class="object-cover w-full h-48 sm:h-64 md:w-80 md:h-auto rounded-t-lg md:rounded-l-lg shadow-md"
+    <img class="object-cover w-full h-48 sm:h-64 md:w-80 lg:w-[45%] md:h-auto rounded-t-lg md:rounded-l-lg hover:scale-105 duration-300 overflow-hidden"
          src="{{ asset($image) }}" alt="{{ $title }}">
 
     <!-- Content -->
     <div class="flex flex-col justify-between p-4 md:p-6 leading-normal z-10 w-full md:w-3/4">
         <!-- Title -->
-        <h5 class="mb-2 text-xl sm:text-2xl md:text-3xl font-extrabold tracking-tight text-gray-900">
+        <h5 class="mb-2 text-xl sm:text-2xl md:text-3xl lg:text-2xl font-extrabold tracking-tight text-gray-900">
             {{ $title }}
         </h5>
         <!-- Divider -->
         <hr class="mb-3 border-gray-300">
         <!-- Description -->
-        <p class="mb-3 text-sm sm:text-base md:text-lg font-medium text-gray-700">
+        <p class="mb-3 text-sm sm:text-base md:text-lg lg:text-md font-medium text-gray-700">
             {{ $short_des }}
         </p>
         <!-- Button -->
-        <div class="card-actions mt-auto flex justify-center">
+        <div class="card-actions mt-auto flex justify-center lg:">
             <button class="w-full max-w-md bg-gradient-to-tr from-blue-500 via-purple-500 to-pink-500 text-white
                 hover:from-blue-600 hover:to-pink-600 hover:shadow-lg rounded-full py-2 sm:py-3 flex items-center justify-center
                 space-x-4 shadow-md transition-all duration-300 ease-in-out" href={{route('single-club',$route)}} wire:navigate>
--- resources/views/livewire/pages/campus-life/all-clubs.blade.php
+++ resources/views/livewire/pages/campus-life/all-clubs.blade.php
@@ -1,13 +1,12 @@
-<div>
-    <div class="mx-auto my-5 px-2 shadow-gray-500 mb-16 rounded lg:w-[80%]">
+<body class="bg-gradient-to-r from-gray-100 via-white to-gray-100 bg-opacity-30 backdrop-blur-3xl">
+    <div class="mx-auto my-5 px-2 mb-16 rounded lg:w-[90%]">
         <h1 class="text-5xl font-bold text-center pt-10">Clubs</h1>
         <hr class="w-48 h-1 mx-auto my-2 bg-emerald-600 mb-16 border-0 rounded md:my-10">
 
-        <div class="grid gap-8 grid-cols-1 md:grid-cols-1 lg:grid-cols-2 mb-6">
+        <div class="grid gap-6 grid-cols-1 md:grid-cols-1 lg:grid-cols-2 mb-6">
             @foreach ($clubs as $club )
             <x-card.club-page-card :image="$club['image']" :title="$club['title']" :short_des="$club['short_des']" :route="$club['route']"/>
             @endforeach
-
         </div>
     </div>
-</div>
+</body>
