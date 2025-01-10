8549e323986569459b5d77a03e1ac660b4dd95a6
Jakiur1234 <70786208+Jakiur1234@users.noreply.github.com>
2025-01-10 23:23:53 +0600

    Fixed issues

--- app/Livewire/Partials/Home/Hero.php
+++ app/Livewire/Partials/Home/Hero.php
@@ -6,31 +6,18 @@ use Livewire\Component;
 
 class Hero extends Component
 {
-    public $images = [];
+    public $image = [];
 
     public function mount()
     {
-        $this->images = [
-            [
-                'src' => asset('images/hero-img1.jpg'),
-                'title' => 'Welcome to Faridpur Engineering College',
-                'description' => 'A Place to learn and Grow.',
-                'style' => 'absolute',
-            ],
-            [
-                'src' => asset('images/hero-img1.jpg'),
-                'title' => 'Where ambition meets opportunity.',
-                'description' => 'Join us in our journey.',
-                'style' => 'absolute bottom-0 left-0 px-6 py-7 my-5'
-            ],
-            [
-                'src' => asset('images/hero-img1.jpg'),
-                'title' => 'For the bold minds and boundless dreams',
-                'description' => 'Embrace the future of education.',
-                'style' => 'absolute bottom-0 left-0 px-6 py-7 my-5'
-            ]
+        $this->image = [
+            'src' => asset('images/hero-img1.jpg'),
+            'title' => 'Welcome to Faridpur Engineering College',
+            'description' => 'A Place to learn and Grow.',
+            'style' => 'absolute',
         ];
     }
+
     public function render()
     {
         return view('livewire.partials.home.hero');
