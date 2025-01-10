3f47c64e3733faf65695a0daa9c4cdee65d63bc9
Jakiur1234 <70786208+Jakiur1234@users.noreply.github.com>
2025-01-10 10:30:37 +0600

    Sticky header

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,25 @@ import Alpine from 'alpinejs';
 document.addEventListener("livewire:navigated", function() {
     window.Alpine = Alpine;
     Alpine.start();
+
+    // navigation menu
+    navBarSticky();
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    navBarSticky();
+});
 
+function navBarSticky(){
+    const navBar = document.getElementById('navBar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 250) {
+            navBar.classList.remove('absolute', 'left-1/2', 'transform', '-translate-x-1/2', '-translate-y-1/2');
+            navBar.classList.add('sticky', 'top-0', 'left-0', 'transform-none');
+        } else {
+            navBar.classList.add('absolute', 'left-1/2', 'transform', '-translate-x-1/2', '-translate-y-1/2');
+            navBar.classList.remove('sticky', 'top-0', 'left-0', 'transform-none');
+        }
+    });
+}
--- resources/views/components/partials/navigation.blade.php
+++ resources/views/components/partials/navigation.blade.php
@@ -1,22 +1,22 @@
 <!-- Menu Links -->
-<nav
-    class="hidden container w-[1170px] mx-auto bg-white text-gray-800 px-4 py-7 rounded shadow-lg md:flex justify-evenly text-lg md:text-base md:shadow-lg z-50 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+<nav id="navBar"
+    class="hidden container w-[1170px] mx-auto bg-white text-gray-800 px-4 py-7 rounded shadow-lg md:flex justify-evenly text-lg md:text-base md:shadow-lg z-50 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500 ease-in"
     x-data="{ open: false, dropdownOpen: false, currentDropdown: null }">
     @php $menus = config('menu.main_menu') @endphp
-        <div class="container mx-auto -m-3 flex justify-between md:justify-center items-center">
-            <x-partials.desktop-menu>
-                @foreach ($menus as $key => $menu)
-                    <x-menu :isDropdown="$menu['isDropdown']" :dropdowns="$menu['isDropdown'] ? $menu['dropdowns'] : []" :route="$menu['route']" :slug="$key"
-                        title="{{ $menu['title'] }}" />
-                @endforeach
-            </x-partials.desktop-menu>
-        </div>
-        <x-partials.mobile-menu>
+    <div class="container mx-auto -m-3 flex justify-between md:justify-center items-center">
+        <x-partials.desktop-menu>
             @foreach ($menus as $key => $menu)
-                <div class="border-t border-gray-100 hover:bg-bgColorLighter hover:text-black">
-                    <x-menu :mobile="true" :isDropdown="$menu['isDropdown']" :dropdowns="$menu['isDropdown'] ? $menu['dropdowns'] : []" :slug="$key" :route="$menu['route']"
-                        title="{{ $menu['title'] }}" />
-                </div>
+                <x-menu :isDropdown="$menu['isDropdown']" :dropdowns="$menu['isDropdown'] ? $menu['dropdowns'] : []" :route="$menu['route']" :slug="$key"
+                    title="{{ $menu['title'] }}" />
             @endforeach
-        </x-partials.mobile-menu>
+        </x-partials.desktop-menu>
+    </div>
+    <x-partials.mobile-menu>
+        @foreach ($menus as $key => $menu)
+            <div class="border-t border-gray-100 hover:bg-bgColorLighter hover:text-black">
+                <x-menu :mobile="true" :isDropdown="$menu['isDropdown']" :dropdowns="$menu['isDropdown'] ? $menu['dropdowns'] : []" :slug="$key" :route="$menu['route']"
+                    title="{{ $menu['title'] }}" />
+            </div>
+        @endforeach
+    </x-partials.mobile-menu>
 </nav>
