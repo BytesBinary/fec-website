7d059b253ea683b2c6a28954a224399211bd5fda
Jakiur1234 <70786208+Jakiur1234@users.noreply.github.com>
2025-01-18 18:55:20 +0600

    Add short details in frontend

--- app/Livewire/Partials/Home/ShortDetails.php
+++ app/Livewire/Partials/Home/ShortDetails.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Partials\Home;
 
+use App\Models\Options;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class ShortDetails extends Component
@@ -9,28 +11,23 @@ class ShortDetails extends Component
     public $details = [];
     public function mount()
     {
-        $this->details = array(
-            [
-                'name' => 'Established',
-                'url' => 'images/establish.png',
-                'number' => '2010',
-            ],
-            [
-                'name' => 'Students',
-                'url' => 'images/students.png',
-                'number' => '800',
-            ],
-            [
-                'name' => 'Departments',
-                'url' => 'images/building.png',
-                'number' => '3',
-            ],
-            [
-                'name' => 'Faculty Members',
-                'url' => 'images/faculty-members.png',
-                'number' => '30',
-            ]
-        );
+        $this->details = Options::where('option_type', 'short-details')
+            ->get()
+            ->map(function ($option) {
+                if($option->option_name === 'established_year'){
+                    $option->logo = 'images/establish.png';
+                } else if( $option->option_name === 'total_students' ){
+                    $option->logo = 'images/students.png';
+                } else if( $option->option_name === 'total_departments' ){
+                    $option->logo = 'images/building.png';
+                } else if( $option->option_name === 'faculty_members' ){
+                    $option->logo = 'images/faculty-members.png';
+                }
+
+                $option->title = Str::replace('_', ' ', Str::title($option->option_name));
+
+                return $option;
+            });
     }
     public function render()
     {
