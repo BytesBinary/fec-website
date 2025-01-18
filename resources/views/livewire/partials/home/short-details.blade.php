409f43b35fe13bca5458c3b0851e263cbb58bb5c
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2025-01-18 17:23:52 +0600

    color

--- resources/views/livewire/partials/home/short-details.blade.php
+++ resources/views/livewire/partials/home/short-details.blade.php
@@ -1,5 +1,5 @@
 <!-- Image and Stats Section -->
-<div class="relative bg-fixed bg-cover bg-center py-16 bg-violet-800">
+<div class="relative bg-fixed bg-cover bg-center py-16 bg-[#f2f0ef]">
     <!-- Overlay -->
     <div class="absolute inset-0 bg-black opacity-50 z-0"></div>
     <!-- Heading Section -->
